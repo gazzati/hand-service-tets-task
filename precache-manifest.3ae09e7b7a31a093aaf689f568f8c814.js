eebc7498c14dfa43182a999a3fef1e3ef0a4449f
Timur <gazzaevtimur@gmail.com>
2020-09-09 16:42:26 +0300

    Updates

---
+++ precache-manifest.3ae09e7b7a31a093aaf689f568f8c814.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db3d179c2087cd567eafd1df6261a2e",
+    "url": "/gazzati/hand-service-tets-task/index.html"
+  },
+  {
+    "revision": "1fa5938351636da6fd10",
+    "url": "/gazzati/hand-service-tets-task/static/css/main.97ca1e1b.chunk.css"
+  },
+  {
+    "revision": "6fb4366c9051aee9cb75",
+    "url": "/gazzati/hand-service-tets-task/static/js/2.f1b79297.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gazzati/hand-service-tets-task/static/js/2.f1b79297.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fa5938351636da6fd10",
+    "url": "/gazzati/hand-service-tets-task/static/js/main.3376122c.chunk.js"
+  },
+  {
+    "revision": "e2d6cd06e7a9778efd82",
+    "url": "/gazzati/hand-service-tets-task/static/js/runtime-main.e9701ef5.js"
+  },
+  {
+    "revision": "b941947c9ddcca68c751b6afa0bda5b4",
+    "url": "/gazzati/hand-service-tets-task/static/media/chick.b941947c.svg"
+  },
+  {
+    "revision": "17f5b7f17896474dfc469ca37ee998af",
+    "url": "/gazzati/hand-service-tets-task/static/media/fire.17f5b7f1.svg"
+  },
+  {
+    "revision": "07d408e40f7b081dc5e22bdf530979b8",
+    "url": "/gazzati/hand-service-tets-task/static/media/skull.07d408e4.svg"
+  }
+]);
